128a7c636bbb8df581069bbb567e9540e6416fb3
Alexey Zakharov <alexey.zakharov@altoros.com>
2019-04-24 23:34:02 +0300

    Typo

--- deployment/ci/task.sh
+++ deployment/ci/task.sh
@@ -7,12 +7,12 @@ set -x
 popd
 
 CF_PASS=$(credhub get -n /bosh-cf01/cf/cf_admin_password | grep ^value | awk '{print $2}')
-credhub set -v /bosh-cf01/logsearch/cf_admin_password -t value -v ${CF_PASS}
+credhub set -n /bosh-cf01/logsearch/cf_admin_password -t value -v ${CF_PASS}
 UAA_PASS=$(credhub get -n /bosh-cf01/cf/uaa_admin_client_secret | grep ^value | awk '{print $2}')
-credhub set -v /bosh-cf01/logsearch/uaa_admin_client_secret -t value -v ${UAA_PASS}
+credhub set -n /bosh-cf01/logsearch/uaa_admin_client_secret -t value -v ${UAA_PASS}
 UUID=$(cat /proc/sys/kernel/random/uuid)
-credhub set -v /bosh-cf01/logsearch/cf-kibana_client_secret -t value -v ${UUID}
-credhub set -v /bosh-cf01/logsearch/system_domain -t value -v ${SYSTEM_DOMAIN}
+credhub set -n /bosh-cf01/logsearch/cf-kibana_client_secret -t value -v ${UUID}
+credhub set -n /bosh-cf01/logsearch/system_domain -t value -v ${SYSTEM_DOMAIN}
 
 arguments=""
 for op in ${OPS_FILES}
