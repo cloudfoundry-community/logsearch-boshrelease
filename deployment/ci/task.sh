d8eb50aaa8c8243b3eb562a171bcb919064d5452
Alexey Zakharov <alexey.zakharov@altoros.com>
2019-04-24 23:17:03 +0300

    Added credhub calls

--- deployment/ci/task.sh
+++ deployment/ci/task.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -eux
 
-cd bbl-state/bbl-state
+pushd bbl-state/bbl-state
 eval "`bbl print-env`"
+popd
 
-bosh -d logsearch deploy logsearch-boshrelease/deployment/logsearch-deployment.yml \
-  -o logsearch-boshrelease/deployment/operations/${OPS_FILES} \
-  -v system_domain=${SYSTEM_DOMAIN} \
-#  -v cf_admin_password="" \
-#  -v uaa_admin_client_secret="" \
-#  -v cf-kibana_client_secret="" \
+CF_PASS=$(credhub get -n /bosh-cf01/cf/cf_admin_password | grep ^value | awk '{print $2}')
+UAA_PASS=$(credhub get -n /bosh-cf01/cf/uaa_admin_client_secret | grep ^value | awk '{print $2}')
+UUID=$(cat /proc/sys/kernel/random/uuid)
+
+local arguments
+arguments="-v system_domain=${SYSTEM_DOMAIN} \
+-v cf_admin_password=${CF_PASS} \
+-v uaa_admin_client_secret=${UAA_PASS} \
+-v cf-kibana_client_secret=${UUID}"
+
+for op in ${OPS_FILES}
+do
+  arguments="${arguments} -o logsearch-boshrelease/deployment/${op}"
+done
+
+bosh -d logsearch deploy logsearch-boshrelease/deployment/logsearch-deployment.yml ${arguments}
