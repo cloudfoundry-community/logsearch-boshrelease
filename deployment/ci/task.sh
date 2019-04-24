d64646cac2dbc17bacc4a9a83d89e2151503046f
Alexey Zakharov <alexey.zakharov@altoros.com>
2019-04-24 23:19:43 +0300

    Securing bbl output

--- deployment/ci/task.sh
+++ deployment/ci/task.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -eux
 
 pushd bbl-state/bbl-state
+set +x
 eval "`bbl print-env`"
+set -x
 popd
 
 CF_PASS=$(credhub get -n /bosh-cf01/cf/cf_admin_password | grep ^value | awk '{print $2}')
 UAA_PASS=$(credhub get -n /bosh-cf01/cf/uaa_admin_client_secret | grep ^value | awk '{print $2}')
 UUID=$(cat /proc/sys/kernel/random/uuid)
 
-local arguments
 arguments="-v system_domain=${SYSTEM_DOMAIN} \
 -v cf_admin_password=${CF_PASS} \
 -v uaa_admin_client_secret=${UAA_PASS} \
