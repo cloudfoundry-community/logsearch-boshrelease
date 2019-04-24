b0d3cd7fc50d565961d1a40ffe5de0be3dbf9474
Alexey Zakharov <alexey.zakharov@altoros.com>
2019-04-24 23:31:12 +0300

    Move vars to credhub

--- deployment/ci/task.sh
+++ deployment/ci/task.sh
@@ -7,17 +7,17 @@ set -x
 popd
 
 CF_PASS=$(credhub get -n /bosh-cf01/cf/cf_admin_password | grep ^value | awk '{print $2}')
+credhub set -v /bosh-cf01/logsearch/cf_admin_password -t value -v ${CF_PASS}
 UAA_PASS=$(credhub get -n /bosh-cf01/cf/uaa_admin_client_secret | grep ^value | awk '{print $2}')
+credhub set -v /bosh-cf01/logsearch/uaa_admin_client_secret -t value -v ${UAA_PASS}
 UUID=$(cat /proc/sys/kernel/random/uuid)
+credhub set -v /bosh-cf01/logsearch/cf-kibana_client_secret -t value -v ${UUID}
+credhub set -v /bosh-cf01/logsearch/system_domain -t value -v ${SYSTEM_DOMAIN}
 
-arguments="-v system_domain=${SYSTEM_DOMAIN} \
--v cf_admin_password=${CF_PASS} \
--v uaa_admin_client_secret=${UAA_PASS} \
--v cf-kibana_client_secret=${UUID}"
-
+arguments=""
 for op in ${OPS_FILES}
 do
   arguments="${arguments} -o logsearch-boshrelease/deployment/${op}"
 done
 
-bosh -d logsearch deploy logsearch-boshrelease/deployment/logsearch-deployment.yml ${arguments}
+bosh -n -d logsearch deploy logsearch-boshrelease/deployment/logsearch-deployment.yml ${arguments}
